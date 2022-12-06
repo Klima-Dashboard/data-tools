323e85dbf102ba4646037434ae340161e9bf4483
Mats Heemeyer <meytin@protonmail.com>
2022-12-06 11:14:32 +0100

    Integrate grafana and node-red with keycloak

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 cp /settings.js /data/settings.js
 export NODE_RED_CREDENTIAL_SECRET=$(cat /run/secrets/node-red-credential-secret) 
+export CLIENT_SECRET=$(cat /run/secrets/node-red-keycloak-client-secret)
 npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
