6c673d6249b7af0a21bf05c7079d8049cad7b119
Mats Heemeyer <meytin@protonmail.com>
2023-05-16 13:10:15 +0200

    Install node-red plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 cp /settings.js /data/settings.js
 export NODE_RED_CREDENTIAL_SECRET=$(cat /run/secrets/node-red-credential-secret) 
 export CLIENT_SECRET=$(cat /run/secrets/node-red-keycloak-client-secret)
+cd /data 
+npm i node-red-contrib-influxdb node-red-contrib-loop-processing node-red-contrib-zip
+cd
 npm --no-update-notifier --no-fund start --cache /data/.npm -- --userDir /data
